bfc631595699b8b3ae6e818a284e726de5fc354b
Nikhil Ravi <nr337@cornell.edu>
2023-11-28 16:32:16 -0500

    Reorganizing components

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import { projects } from "@/content/projectsData";
+import Project from "./ProjectItem";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+
+function ProjectsList() {
+  return (
+    <ResponsiveMasonry columnsCountBreakPoints={{ 250: 1, 450: 2 }}>
+      <Masonry gutter="25px">
+        {projects.map((project, index) => (
+          <Project key={project.title} item={project} />
+        ))}
+      </Masonry>
+    </ResponsiveMasonry>
+  );
+}
+
+export default ProjectsList;
